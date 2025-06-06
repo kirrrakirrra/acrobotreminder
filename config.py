22d9fb49d35b1af82674d3bfc73640263f5838cd
kirrrakirrra <kirrra419904@gmail.com>
2025-06-06 18:16:22 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# Токены и ID
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = 986242491
+NOTIFY_ID = 1291715324
+GROUP_ID = -1001820363527
+
+# Файлы
+ABON_FILE = "abons.json"
+LOG_FILE = "bot.log"
+
+# Часовой пояс (Вьетнам UTC+7)
+TIMEZONE_OFFSET = 7
