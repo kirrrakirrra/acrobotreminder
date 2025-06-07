28c9b318dd7fdf28b16af49c835feab8affb870e
kirrrakirrra <kirrra419904@gmail.com>
2025-06-07 12:17:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,8 @@ async def main():
     await application.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import nest_asyncio
+    import asyncio
+
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
