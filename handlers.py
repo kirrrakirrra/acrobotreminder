39446bf9f447a11635fed3b8cacbce1d5d92731c
kirrrakirrra <kirrra419904@gmail.com>
2025-06-07 11:42:39 +0700

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -2,8 +2,5 @@ from telegram.ext import Application, CallbackQueryHandler
 from notify import handle_callback_query
 
 def register_handlers(application: Application):
-    # Все команды (/start, /add и т.д.)
-    register_commands(application)
-
-    # Обработка нажатий на inline-кнопки (из notify.py)
+    # Обработка inline-кнопок (Да / Нет / причины отмены)
     application.add_handler(CallbackQueryHandler(handle_callback_query))
